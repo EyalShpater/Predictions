bb9b3af6f4a5b16a5f065f831ee5d94c6d591dfb
Eyal Shpater <shpaters@hotmail.com>
2023-08-05 05:11:46 +0300

    Activation

--- logic/src/simulation/propety/law/rule/Activation.java
+++ logic/src/simulation/propety/law/rule/Activation.java
@@ -1,6 +1,32 @@
 package simulation.propety.law.rule;
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+
 public class Activation {
     private int numOfTicksToActivate;
-    private double probabilityToActivate;
+    private double probabilityToActivate = 1;
+
+    public int getNumOfTicksToActivate() {
+        return numOfTicksToActivate;
+    }
+
+    public void setNumOfTicksToActivate(int numOfTicksToActivate) {
+        if (numOfTicksToActivate < 0) {
+            throw new IllegalArgumentException("Ticks can not be negative!");
+        }
+
+        this.numOfTicksToActivate = numOfTicksToActivate;
+    }
+
+    public double getProbabilityToActivate() {
+        return probabilityToActivate;
+    }
+
+    public void setProbabilityToActivate(double probability) {
+        if (probability < 0 || probability > 1) {
+            throw new IllegalArgumentException("Probability must be between 0 to 1");
+        }
+
+        this.probabilityToActivate = probability;
+    }
 }
--- logic/src/simulation/propety/law/rule/Rule.java
+++ logic/src/simulation/propety/law/rule/Rule.java
@@ -8,4 +8,18 @@ public class Rule {
     private String title;
     private ArrayList<Action> actions;
     private Activation activation;
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        str.append(title).append(System.lineSeparator());
+        str.append(activation).append(System.lineSeparator());
+        str.append(actions.size()).append("# actions:").append(System.lineSeparator());
+        for (Action action : actions) {
+            str.append(action).append(System.lineSeparator());
+        }
+
+        return str.toString();
+    }
 }
