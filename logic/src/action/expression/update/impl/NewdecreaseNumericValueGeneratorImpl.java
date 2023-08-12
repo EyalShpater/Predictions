4ac6b09cf9d656de677091ca64fb117460f4bddf
Shavit Shelli <shavit.shelli57@gmail.com>
2023-08-12 10:24:45 +0300

    added deacreaseAction based on IncreaseAction logic added an abstract class for calcs

--- logic/src/action/expression/update/impl/NewdecreaseNumericValueGeneratorImpl.java
+++ logic/src/action/expression/update/impl/NewdecreaseNumericValueGeneratorImpl.java
@@ -6,7 +6,9 @@ import action.expression.update.api.NewNumericValueGenerator;
 public class NewdecreaseNumericValueGeneratorImpl extends AbstractNewNumericValueGenerator implements NewNumericValueGenerator {
     @Override
     public Object calcUpdatedValue(Object expressionToConvert, Object propertyValue) {
-        //TODO: Need to add a property range check
+        //---------------------TODO WITH EYAL----------------
+        //TODO: Need to add a property range check -> might want to pass propertyInstance to this function
+        // TODO: Need to add type check -> if propertyValue is int it cant get double
         if (propertyValue instanceof Integer && expressionToConvert instanceof Integer) {
             return ((Integer) propertyValue - (Integer) expressionToConvert);
         } else if (propertyValue instanceof Integer && expressionToConvert instanceof Double) {
