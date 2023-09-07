b7ab811948f980cff5ed7b5ab9b244a06c29ff43
shpater1234 <shpaters@hotmail.com>
2023-09-07 04:33:42 +0300

    added Proximity class (not implemented)

---
+++ Logic/src/action/impl/Proximity.java
@@ -0,0 +1,26 @@
+package action.impl;
+
+import action.api.AbstractAction;
+import action.api.Action;
+import action.api.ActionType;
+import action.context.api.Context;
+import definition.entity.api.EntityDefinition;
+import impl.ActionDTO;
+
+import java.util.Map;
+
+public class Proximity extends AbstractAction {
+    public Proximity(EntityDefinition mainEntity) {
+        super(mainEntity, ActionType.PROXIMITY);
+    }
+
+    @Override
+    public void invoke(Context context) {
+
+    }
+
+    @Override
+    public Map<String, String> getArguments() {
+        return null;
+    }
+}
